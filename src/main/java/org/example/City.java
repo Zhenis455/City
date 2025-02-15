54bdcbcf2eef56db0699bc063f245fcdf517a049
Zhenis <zenismederbek@gmail.com>
2025-02-15 11:40:23 +0500

    fifth commit

--- src/main/java/org/example/City.java
+++ src/main/java/org/example/City.java
@@ -1,6 +1,27 @@
 package org.example;
 
-public record City(String cityName, int population) {
+public class City {
+    private String cityName;
+    private int population;
 
+    public City(String cityName, int population) {
+        this.cityName = cityName;
+        this.population = population;
+    }
 
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public int getPopulation() {
+        return population;
+    }
+
+    public void setPopulation(int population) {
+        this.population = population;
+    }
 }
