569d9f3d1f025b67519eef72723464785cf4160f
Zhenis <zenismederbek@gmail.com>
2025-02-15 11:30:31 +0500

    fourth commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -10,11 +10,8 @@ public class Main {
         
         City cityService1 = context.getBean("cityService1", City.class);
         City cityService2 = context.getBean("cityService2", City.class);
-        City cityService3 = context.getBean("cityService3", City.class);
-        City cityService4 = context.getBean("cityService4", City.class);
+    
         System.out.println("Город: " + cityService1.getCityName() + ", Население: " + cityService1.getPopulation());
         System.out.println("Город: " + cityService2.getCityName() + ", Население: " + cityService2.getPopulation());
-        System.out.println("Город: " + cityService3.getCityName() + ", Население: " + cityService3.getPopulation());
-        System.out.println("Город: " + cityService4.getCityName() + ", Население: " + cityService4.getPopulation());
-    }
+           }
 }
