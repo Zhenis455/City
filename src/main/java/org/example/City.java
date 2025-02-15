804b644bad7916898ab427eb610a738f99a4c77d
Zhenis <zenismederbek@gmail.com>
2025-02-15 11:33:35 +0500

    fourth commit

--- src/main/java/org/example/City.java
+++ src/main/java/org/example/City.java
@@ -12,9 +12,16 @@ public class City {
     public String getCityName() {
         return cityName;
     }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
     public int getPopulation() {
         return population;
     }
 
-
+    public void setPopulation(int population) {
+        this.population = population;
+    }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,12 +6,13 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class Main {
     public static void main(String[] args) {
         ApplicationContext context = new AnnotationConfigApplicationContext(ProjectConfig.class);
-
-        
         City cityService1 = context.getBean("cityService1", City.class);
         City cityService2 = context.getBean("cityService2", City.class);
-    
+        City cityService3 = context.getBean("cityService3", City.class);
+        City cityService4 = context.getBean("cityService4", City.class);
         System.out.println("Город: " + cityService1.getCityName() + ", Население: " + cityService1.getPopulation());
         System.out.println("Город: " + cityService2.getCityName() + ", Население: " + cityService2.getPopulation());
-           }
+        System.out.println("Город: " + cityService3.getCityName() + ", Население: " + cityService3.getPopulation());
+        System.out.println("Город: " + cityService4.getCityName() + ", Население: " + cityService4.getPopulation());
+    }
 }
