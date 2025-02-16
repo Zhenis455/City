116b73400b750c37ec46c3f79c790000736167de
Zhenis <zenismederbek@gmail.com>
2025-02-16 18:12:44 +0500

    second commit

--- src/main/java/org/example/Profession.java
+++ src/main/java/org/example/Profession.java
@@ -1,9 +1,17 @@
 package org.example;
+
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Profession {
-    private String name = "Программист";
+    private String name;
+
+    public Profession() {}
+
+    public Profession(String name) {
+        this.name = name;
+    }
 
     public String getName() {
         return name;
@@ -11,5 +19,20 @@ public class Profession {
 
     public void setName(String name) {
         this.name = name;
+    }
+}
+
+@Component
+@Primary
+class Programmer extends Profession {
+    public Programmer() {
+        super("Программист");
+    }
+}
+
+@Component
+class Designer extends Profession {
+    public Designer() {
+        super("Дизайнер");
     }
 }
--- src/main/java/org/example/ProjectConfig.java
+++ src/main/java/org/example/ProjectConfig.java
@@ -1,12 +1,41 @@
 package org.example;
 
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 @ComponentScan(basePackages = "org.example")
 public class ProjectConfig {
 
+    @Bean
+    public List<Student> students() {
+        List<Student> students = new ArrayList<>();
 
+        Student s1 = new Student(new org.example.Programmer());
+        s1.setName("Иван Иванов");
+
+        Student s2 = new Student(new org.example.Programmer());
+        s2.setName("Петр Петров");
+
+        Student s3 = new Student(new org.example.Programmer());
+        s3.setName("Сергей Сергеев");
+
+        Student s4 = new Student(new org.example.Designer());
+        s4.setName("Анна Смирнова");
+
+        Student s5 = new Student(new org.example.Designer());
+        s5.setName("Мария Козлова");
+
+        students.add(s1);
+        students.add(s2);
+        students.add(s3);
+        students.add(s4);
+        students.add(s5);
+
+        return students;
+    }
 }
